52223fd089ad92733703b5f95c7f0674efdcad19
elias174 <elias.valencia@ucsp.edu.pe>
2018-06-04 19:00:26 -0500

    added results

--- main.cpp
+++ main.cpp
@@ -7,10 +7,22 @@ using namespace std;
 int main()
 {
     LinkageOperation *simple = new SimpleLinkage();
-    LinkageCluster l(string("test_dataset"));
-    l.print_matrix();
+    LinkageOperation *average = new AverageLinkage();
+    LinkageOperation *complete = new CompleteLinkage();
+
+    LinkageCluster l(string("diauxic.txt"));
+    //l.print_matrix();
     cout << endl << endl;
-    //l.make_clustering(5,simple);
-    l.disociative_clustering(2);
+
+    cout << "Simple linkage with 200 clusters" << endl;
+    auto started = std::chrono::high_resolution_clock::now();
+    l.make_clustering(200,simple);
+    auto done = std::chrono::high_resolution_clock::now();
+    cout << endl;
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(done-started).count() << " microseconds " << endl;
+    cout << endl << endl;
+
+
+    //l.disociative_clustering(2);
     return 0;
 }
